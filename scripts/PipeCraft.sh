71b85f135229c943be2bf8341a0a0c708cd6f771
Brooks J Rady <b.j.rady@gmail.com>
2020-07-21 21:31:59 +0100

    Add some intense options

--- scripts/PipeCraft.sh
+++ scripts/PipeCraft.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Command for launching the Minecraft server
-MCCMD="java -Xmx1024M -Xms1024M -jar server.jar -nogui"
+MCCMD="ionice -c 1 -n 0 nice -20 java -Xmx4096M -Xms2048M -jar server.jar -nogui"
 
 # Command for launching the Bot
 BOTCMD="./blocky"
